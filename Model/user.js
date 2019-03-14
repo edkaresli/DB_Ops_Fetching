b8a07f5780e225449770e43e0821978424077689
Eduard Karesli <ed.karesli@gmail.com>
2019-03-14 23:37:02 +0100

    Adding DB configuration file, setup and User model

---
+++ Model/user.js
@@ -0,0 +1,33 @@
+
+
+
+class User {
+
+  consutrctor(uname, nme, email, web) {
+      this.uname = uname;
+      this.name  = nme;
+      this.email = email;
+      this.web   = web;
+  }
+
+  insertUser() {
+
+  }
+
+  // static??
+  findUser(uname) {
+
+  }
+
+  // static??
+  updateUser(uname) {
+
+  }
+  
+  // static??
+  deleteUser(uname) {
+
+  }
+}
+
+module.exports = User;
